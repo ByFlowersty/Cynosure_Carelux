3489ff1eed94af7dc188a174df918991028f1f57
ByFlowersty <a.flores.c141@edu.utc.mx>
2025-07-30 19:50:39 -0600

    Update EREBUS.tsx

--- src/components/paciente/EREBUS.tsx
+++ src/components/paciente/EREBUS.tsx
@@ -1,6 +1,6 @@
 // src/components/paciente/EREBUS.tsx // Asumiendo ubicación dentro de paciente
-import React, { useState, useRef, useCallback, useEffect } from 'react';
-import { Mic, Square, Loader2, AlertCircle, Play, Pause, BrainCircuit } from 'lucide-react'; // Iconos relevantes
+import  { useState, useRef, useCallback, useEffect } from 'react';
+import { Mic, Square, Loader2, AlertCircle,  Play, Pause, BrainCircuit } from 'lucide-react'; // Iconos relevantes
 
 // Interfaz para la respuesta esperada de la API
 interface ApiResponse {
